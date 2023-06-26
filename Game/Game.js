a166e7f58f41bd31782e49cb5a623c2ef0af114a
ArisoaNc <rmnicole48@gmail.com>
2023-06-26 17:40:56 +0200

    Project update

--- Game/Game.js
+++ Game/Game.js
@@ -2,31 +2,59 @@ window.onload=()=>{
     
     let arrow= document.getElementsByClassName('arrow2');
     arrow[0].addEventListener('click', function(){
-        window.scrollTo(top, 'smooth');
+    window.scrollTo(top, 'smooth');
 
     });
     arrow[1].addEventListener('click', function(){
-        window.scrollBy(0, window.innerHeight);
+    window.scrollBy(0, window.innerHeight);
     });
 };
 $(document).ready(function(){
-    // $.ajax({
-    //     url:"https://batman-api.sayna.space/questions",
-    //     datatype: 'json',
-    //     success : function(questions){
-    //         console.log(questions[5].response[0].isGood);
+    const demarrer = document.getElementById('demarrer');
+    const nextbutton = document.getElementById('question-suivant');
 
-    //     },
-    //     error: function(){
-    //         console.log('impossible');
-    //     }
-    $('#btn').click(function(){
-        $('*').fadeIN(slow);
+    demarrer.addEventListener('click',(e)=>{
+    e.preventDefault();
+    showQuestions();
+    $.ajax({
+        url:"https://batman-api.sayna.space/questions",
+        datatype: 'json',
+        success: function(questions){
+        let totalPoint=0;
+        let currentQuiz = 0;
+        let totalQuiz= questions.lenght;
 
+        $('#total-quiz').text(totalQuiz);
+        setForm(questions[currentQuiz].question,questions[currentQuiz].response)
+    },
+    error: function(){
+        console.log('Un probème est survenu Impossible de se connecter au serveur');
 
-
+    },
+        })
     })
 
-    })
+});
+
+function showQuestions(){
+    const question = document.getElementById('questions');
+    question.style.display = 'block';
+    const downFleche = document.getElementById('down');
+    const demarrer = document.getElementById('entete');
+    demarrer.style.display = 'none';
+    downFleche.style.display = 'none';
+    $('#question-suivante').show();
+   
+    g
+
+}
+
+
+    
+    
+    
+    
+
+    
 
 
--- Home/Home.js
+++ Home/Home.js
@@ -1,19 +1,92 @@
 window.onload=()=>{
     let myForm = document.getElementById('myForm');
+    
+    function show(){
+        location.reload();
+        window.scrollTo(top);
+        }
     myForm.addEventListener('submit',function(e){
        let email=document.getElementById('email');
-        if(email.value==''){
-            e.preventDefault;
+       let check=document.getElementById('check');
+       let select=document.getElementById('deroulantList');
+      
+        if(email.value=='' ){
+            e.preventDefault();
             let invalid=document.getElementById('invalid');
             invalid.innerHTML='Veuiller entrer un adresse email';
             invalid.style.color='red';
             invalid.style.fontSize='15px';
             invalid.style.fontStyle='italic';
         }else{
+            invalid.innerHTML='';
+        }
+        if(!check.checked){
+        e.preventDefault();
+        let invalid1=document.getElementById('invalid1');
+        invalid1.innerHTML='A Cocher ';
+        invalid1.style.color='red';
+        invalid1.style.fontSize='15px';
+        invalid1.style.fontStyle='italic';
+       }else{
+        invalid1.innerHTML='';
+       }
+     if(select.selectedIndex==0){
+        e.preventDefault();
+        
+        let invalid2=document.getElementById('invalid2');
+        invalid2.innerHTML='Veuillez choisir une option';
+        invalid2.style.color='red';
+        invalid2.style.fontSize='15px';
+        invalid2.style.fontStyle='italic';}
+        else{
+            invalid2.innerHTML='';
+           
+        }
+    if( select.selectedIndex!=0 && check.checked&& email.value!=''){
         e.preventDefault();
         document.getElementById('popupbox').style.display='block';
-        invalid.innerHTML='';
-        }
+        setTimeout(show,3000);   
+    }
     })
+
+  
+    $('main .cinema article').mouseenter(() => {
+        // robert_pattinson
+        $('main #cinema article:nth-child(1) img').mouseenter(() => {
+            setTimeout(()=>{
+                $('#robert_pattinson').addClass('show')
+                $('#robert_pattinson').removeClass('information')
+            }, 100)
+        });      // christian_bale
+        $('main #cinema article:nth-child(2) img').mouseenter(() => {
+            setTimeout(()=>{
+                $('#christian_bale').addClass('show')
+                $('#christian_bale').removeClass('information')
+            }, 100)
+        });
+         // ben_affleck
+         $('main #cinema article:nth-child(3) img').mouseenter(() => {
+            setTimeout(()=>{
+                $('#ben_affleck').addClass('show')
+                $('#ben_affleck').removeClass('information')
+            }, 100)
+        });
+ 
+    });
+    // cinema mouse leave
     
-};
+        $('main #cinema article:nth-child(1)').mouseleave(() => {
+            $('#robert_pattinson').addClass('information')
+            $('#robert_pattinson').removeClass('show')
+        });
+        $('main #cinema article:nth-child(2)').mouseleave(() => {
+            $('#christian_bale').addClass('information')
+            $('#christian_bale').removeClass('show')
+        });
+        $('main #cinema article:nth-child(3)').mouseleave(() => {
+            $('#ben_affleck').addClass('information')
+            $('#ben_affleck').removeClass('show')
+        });
+    
+}
+
